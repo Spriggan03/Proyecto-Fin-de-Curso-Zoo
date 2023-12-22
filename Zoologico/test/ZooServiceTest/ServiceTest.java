7bcd923a590e69f1f5194815a6d4e1b7e54c5f36
¨Casa¨ <dububu.0109@gmail.com>
2023-12-21 19:44:15 -0500

    Color caracteristico Test

---
+++ Zoologico/test/ZooServiceTest/ServiceTest.java
@@ -0,0 +1,37 @@
+package ZooServiceTest;
+
+import Modelos.Ave;
+import Modelos.Especie;
+import Services.ZooService;
+import java.util.ArrayList;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Test;
+
+public class ServiceTest {
+
+    @Test
+    public void getColorCaracteristicoEspecieTest() {
+
+//             Arrange (preparacion)
+        ZooService zoo = new ZooService(new ArrayList<>());
+
+        String colorEsperado = ("rojo");
+
+        String nombreEspecie = ("loro");
+
+        Ave a4 = new Ave(2, null, true, null, 2, 2, null, 2, 2, 2, 2, 2);
+
+        //Act (ACCION)
+        a4.setNombre("loro");
+
+        a4.setColorPlumas("rojo");
+
+        zoo.getColorCaracteristicoEspecie(nombreEspecie);
+
+        // Assert(Prueba)     
+        assertEquals(colorEsperado, ("rojo"));
+    }
+  
+}
